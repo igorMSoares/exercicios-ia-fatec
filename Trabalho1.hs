e7b81ba03fb0f01d56e7473970021f19da83861b
igorMSoares <igorms.dev@gmail.com>
2025-08-31 16:41:09 -0300

    Utiliza guards para qualquer entrada diferente de zero ser tratada como 1

--- Trabalho1.hs
+++ Trabalho1.hs
@@ -19,16 +19,28 @@ neuronioMCP w x theta = funcDegrau (dot w x) theta
 
 mcpAND :: (Int, Int) -> Int
 -- weights = [1, 1], theta = 1.5
-mcpAND (x1, x2) = neuronioMCP [1, 1] [fromIntegral x1, fromIntegral x2] 1.5
+mcpAND (x1, x2)
+  | x1 /= 1 && x1 /= 0 = neuronioMCP [1, 1] [1, fromIntegral x2] 1.5
+  | x2 /= 1 && x2 /= 0 = neuronioMCP [1, 1] [fromIntegral x1, 1] 1.5
+  | otherwise = neuronioMCP [1, 1] [fromIntegral x1, fromIntegral x2] 1.5
 
 mcpOR :: (Int, Int) -> Int
 -- weights = [1, 1], theta = 0.5
-mcpOR (x1, x2) = neuronioMCP [1, 1] [fromIntegral x1, fromIntegral x2] 0.5
+mcpOR (x1, x2)
+  | x1 /= 1 && x1 /= 0 = neuronioMCP [1, 1] [1, fromIntegral x2] 0.5
+  | x2 /= 1 && x2 /= 0 = neuronioMCP [1, 1] [fromIntegral x1, 1] 0.5
+  | otherwise = neuronioMCP [1, 1] [fromIntegral x1, fromIntegral x2] 0.5
 
 mcpNAND :: (Int, Int) -> Int
 -- weights = [-1, -1], theta = -1.5
-mcpNAND (x1, x2) = neuronioMCP [-1, -1] [fromIntegral x1, fromIntegral x2] (-1.5)
+mcpNAND (x1, x2)
+  | x1 /= 1 && x1 /= 0 = neuronioMCP [-1, -1] [1, fromIntegral x2] (-1.5)
+  | x2 /= 1 && x2 /= 0 = neuronioMCP [-1, -1] [fromIntegral x1, 1] (-1.5)
+  | otherwise = neuronioMCP [-1, -1] [fromIntegral x1, fromIntegral x2] (-1.5)
 
 mcpNOR :: (Int, Int) -> Int
 -- weights = [-1, -1], theta = -0.5
-mcpNOR (x1, x2) = neuronioMCP [-1, -1] [fromIntegral x1, fromIntegral x2] (-0.5)
+mcpNOR (x1, x2)
+  | x1 /= 1 && x1 /= 0 = neuronioMCP [-1, -1] [1, fromIntegral x2] (-0.5)
+  | x2 /= 1 && x2 /= 0 = neuronioMCP [-1, -1] [fromIntegral x1, 1] (-0.5)
+  | otherwise = neuronioMCP [-1, -1] [fromIntegral x1, fromIntegral x2] (-0.5)
